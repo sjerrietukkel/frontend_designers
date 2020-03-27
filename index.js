7991e2f7d345a49e4a7de012d37cd20607b01609
Daniël Van der Schuur <danielvanderschuur@MacBook-Pro-van-Daniel.local>
2020-03-27 17:10:13 +0100

    filters working, no health yet, styling not finished

--- index.js
+++ index.js
@@ -1,19 +1,139 @@
 const isEmpty = document.getElementById("app").childNodes.length
-// function validate(){
-//     if (document.getElementById('remember').checked){
-//         console.log("checked")
-//     }else{
-//     console.log("Unchecked")
-//     }
-// }    
+// const fname = document.getElementById("fname").value
+// const input = fname.toLowerCase()
+const vegetarian = document.getElementById('vegetarian')
+const student = document.getElementById('student')
+const gezond = document.getElementById('healthy')
 
 if (isEmpty == 1) {
     document.querySelector('#app').insertAdjacentHTML('beforeend', `<div class="full-width"><h1>Vul eerst je voorkeuren in</h1></div><div class="card-loading"></div><div class="card-loading"></div><div class="card-loading"></div>`)
 }
 
-const vegetarian = document.getElementById('vegetarian')
-const student = document.getElementById('student')
-const healthy = document.getElementById('healthy')
+function Submit() {
+    event.preventDefault()
+    if (vegetarian.checked == true & student.checked == false & gezond.checked == false){
+        fetch(`https://api.spoonacular.com/recipes/random?number=3&tags=vegetarian,dinner&apiKey=dabe659634cb4253ae5f4f1a393456f3`)
+        .then((response) => {
+        return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            const html = data.recipes.map (data => {
+                return `<div class="card"><div class="card-head"><img src="${data.image}" alt="recipe image" class="card-image"></div><div class="card-title"><h1>${data.title}</h1><div class="flex-info"><p class="icon-text"><i class="fas fa-clock"></i> ${data.readyInMinutes}m</p><p class="icon-text"><i class="fas fa-users"></i> ${data.servings}</p></div><a href="${data.sourceUrl}" class="link">Bekijk Recept</a></div></div>`
+            });
+            const app = document.getElementById('app')
+            app.innerHTML = ""
+            document.querySelector('#app').insertAdjacentHTML('beforeend', html)
+            console.log("vegetarian")
+    })
+    }else if(vegetarian.checked == true & student.checked == true & gezond.checked == false) {
+        fetch(`https://api.spoonacular.com/recipes/random?number=3&tags=vegetarian,dinner&apiKey=dabe659634cb4253ae5f4f1a393456f3`)
+        .then((response) => {
+        return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            const html = data.recipes.map (data => {
+                return `<div class="card"><div class="card-head"><img src="${data.image}" alt="recipe image" class="card-image"></div><div class="card-title"><h1>${data.title}</h1><div class="flex-info"><p class="icon-text"><i class="fas fa-clock"></i> ${data.readyInMinutes}m</p><p class="icon-text"><i class="fas fa-users"></i> ${data.servings}</p></div><a href="${data.sourceUrl}" class="link">Bekijk Recept</a></div></div>`
+            });
+            const app = document.getElementById('app')
+            app.innerHTML = ""
+            document.querySelector('#app').insertAdjacentHTML('beforeend', html)
+            console.log("vegetarian student")
+    })
+    }else if(vegetarian.checked == true & student.checked == true & gezond.checked == true) {
+        fetch(`https://api.spoonacular.com/recipes/random?number=3&tags=vegetarian,dinner&apiKey=dabe659634cb4253ae5f4f1a393456f3`)
+        .then((response) => {
+        return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            const html = data.recipes.map (data => {
+                return `<div class="card"><div class="card-head"><img src="${data.image}" alt="recipe image" class="card-image"></div><div class="card-title"><h1>${data.title}</h1><div class="flex-info"><p class="icon-text"><i class="fas fa-clock"></i> ${data.readyInMinutes}m</p><p class="icon-text"><i class="fas fa-users"></i> ${data.servings}</p></div><a href="${data.sourceUrl}" class="link">Bekijk Recept</a></div></div>`
+            });
+            const app = document.getElementById('app')
+            app.innerHTML = ""
+            document.querySelector('#app').insertAdjacentHTML('beforeend', html)
+            console.log("vegetarian student gezond")
+    })
+    }else if(vegetarian.checked == true & student.checked == false & gezond.checked == true) {
+        fetch(`https://api.spoonacular.com/recipes/random?number=3&tags=vegetarian,dinner&apiKey=dabe659634cb4253ae5f4f1a393456f3`)
+        .then((response) => {
+        return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            const html = data.recipes.map (data => {
+                return `<div class="card"><div class="card-head"><img src="${data.image}" alt="recipe image" class="card-image"></div><div class="card-title"><h1>${data.title}</h1><div class="flex-info"><p class="icon-text"><i class="fas fa-clock"></i> ${data.readyInMinutes}m</p><p class="icon-text"><i class="fas fa-users"></i> ${data.servings}</p></div><a href="${data.sourceUrl}" class="link">Bekijk Recept</a></div></div>`
+            });
+            const app = document.getElementById('app')
+            app.innerHTML = ""
+            document.querySelector('#app').insertAdjacentHTML('beforeend', html)
+            console.log(" vegetarisch gezond")
+    })
+    }else if(vegetarian.checked == false & student.checked == true & gezond.checked == true) {
+        fetch(`https://api.spoonacular.com/recipes/random?number=3&tags=vegetarian,dinner&apiKey=dabe659634cb4253ae5f4f1a393456f3`)
+        .then((response) => {
+        return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            const html = data.recipes.map (data => {
+                return `<div class="card"><div class="card-head"><img src="${data.image}" alt="recipe image" class="card-image"></div><div class="card-title"><h1>${data.title}</h1><div class="flex-info"><p class="icon-text"><i class="fas fa-clock"></i> ${data.readyInMinutes}m</p><p class="icon-text"><i class="fas fa-users"></i> ${data.servings}</p></div><a href="${data.sourceUrl}" class="link">Bekijk Recept</a></div></div>`
+            });
+            const app = document.getElementById('app')
+            app.innerHTML = ""
+            document.querySelector('#app').insertAdjacentHTML('beforeend', html)
+            console.log(" student gezond")
+    })
+    }else if(vegetarian.checked == false & student.checked == true & gezond.checked == false) {
+        fetch(`https://api.spoonacular.com/recipes/random?number=3&tags=vegetarian,dinner&apiKey=dabe659634cb4253ae5f4f1a393456f3`)
+        .then((response) => {
+        return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            const html = data.recipes.map (data => {
+                return `<div class="card"><div class="card-head"><img src="${data.image}" alt="recipe image" class="card-image"></div><div class="card-title"><h1>${data.title}</h1><div class="flex-info"><p class="icon-text"><i class="fas fa-clock"></i> ${data.readyInMinutes}m</p><p class="icon-text"><i class="fas fa-users"></i> ${data.servings}</p></div><a href="${data.sourceUrl}" class="link">Bekijk Recept</a></div></div>`
+            });
+            const app = document.getElementById('app')
+            app.innerHTML = ""
+            document.querySelector('#app').insertAdjacentHTML('beforeend', html)
+            console.log(" student")
+    })
+    }else if(vegetarian.checked == false & student.checked == false & gezond.checked == true) {
+        fetch(`https://api.spoonacular.com/recipes/random?number=3&tags=vegetarian,dinner&apiKey=dabe659634cb4253ae5f4f1a393456f3`)
+        .then((response) => {
+        return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            const html = data.recipes.map (data => {
+                return `<div class="card"><div class="card-head"><img src="${data.image}" alt="recipe image" class="card-image"></div><div class="card-title"><h1>${data.title}</h1><div class="flex-info"><p class="icon-text"><i class="fas fa-clock"></i> ${data.readyInMinutes}m</p><p class="icon-text"><i class="fas fa-users"></i> ${data.servings}</p></div><a href="${data.sourceUrl}" class="link">Bekijk Recept</a></div></div>`
+            });
+            const app = document.getElementById('app')
+            app.innerHTML = ""
+            document.querySelector('#app').insertAdjacentHTML('beforeend', html)
+            console.log(" gezond")
+    })
+    }else{
+        fetch(`https://api.spoonacular.com/recipes/random?number=3&tags=dinner&apiKey=dabe659634cb4253ae5f4f1a393456f3`)
+        .then((response) => {
+        return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            const html = data.recipes.map (data => {
+                return `<div class="card"><div class="card-head"><img src="${data.image}" alt="recipe image" class="card-image"></div><div class="card-title"><h1>${data.title}</h1><div class="flex-info"><p class="icon-text"><i class="fas fa-clock"></i> ${data.readyInMinutes}m</p><p class="icon-text"><i class="fas fa-users"></i> ${data.servings}</p></div><a href="${data.sourceUrl}" class="link">Bekijk Recept</a></div></div>`
+            });
+            const app = document.getElementById('app')
+            app.innerHTML = ""
+            document.querySelector('#app').insertAdjacentHTML('beforeend', html)
+            console.log("no input")
+        }
+    )}
+}
+
 // function Submit() {
 //     event.preventDefault()
 //     const fname = document.getElementById("fname").value
@@ -39,43 +159,3 @@ const healthy = document.getElementById('healthy')
 
 // 'https://api.spoonacular.com/recipes/random&apiKey=a840e03e4ca140dfb16812ea490c8007'
 // 'https://api.spoonacular.com/recipes/search?instructionsRequired=true?limitLicense=true?query=yogurt&apiKey=a840e03e4ca140dfb16812ea490c8007'
-
-function Submit() {
-    event.preventDefault()
-    const fname = document.getElementById("fname").value
-
-    // const points = document.getElementById("points").value
-    // const persons = document.getElementById("aantalMensen").value
-    if (vegetarian.checked == true){
-        fetch(`https://api.spoonacular.com/recipes/random?number=3&tags=${fname},vegetarian,dinner&apiKey=dabe659634cb4253ae5f4f1a393456f3`)
-        .then((response) => {
-        return response.json()
-        })
-        .then((data) => {
-            console.log(data)
-            const html = data.recipes.map (data => {
-                return `<div class="card"><div class="card-head"><img src="${data.image}" alt="recipe image" class="card-image"></div><div class="card-title"><h1>${data.title}</h1><div class="flex-info"><p class="icon-text"><i class="fas fa-clock"></i> ${data.readyInMinutes}m</p><p class="icon-text"><i class="fas fa-users"></i> ${data.servings}</p></div><a href="${data.sourceUrl}" class="link">Bekijk Recept</a></div></div>`
-            });
-            const app = document.getElementById('app')
-            app.innerHTML = ""
-            document.querySelector('#app').insertAdjacentHTML('beforeend', html)
-            console.log("vegetarian pizza")
-    })
-    }else{
-        fetch(`https://api.spoonacular.com/recipes/random?number=3&tags=${fname},dinner&apiKey=dabe659634cb4253ae5f4f1a393456f3`)
-        .then((response) => {
-        return response.json()
-        })
-        .then((data) => {
-            console.log(data)
-            const html = data.recipes.map (data => {
-                return `<div class="card"><div class="card-head"><img src="${data.image}" alt="recipe image" class="card-image"></div><div class="card-title"><h1>${data.title}</h1><div class="flex-info"><p class="icon-text"><i class="fas fa-clock"></i> ${data.readyInMinutes}m</p><p class="icon-text"><i class="fas fa-users"></i> ${data.servings}</p></div><a href="${data.sourceUrl}" class="link">Bekijk Recept</a></div></div>`
-            });
-            const app = document.getElementById('app')
-            app.innerHTML = ""
-            document.querySelector('#app').insertAdjacentHTML('beforeend', html)
-            console.log("meat pizza")
-    }
-        )}
-}
-
